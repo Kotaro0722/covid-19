c3be3563d791119c7374761154bb89074c55d06a
T121100 <edt121100.gmail.com>
2024-07-07 22:59:41 +0900

    へんこう

--- app/__init__.py
+++ app/__init__.py
@@ -25,5 +25,17 @@ def create_app():
     
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-    
+
+    from .action import action as action_blueprint
+    app.register_blueprint(action_blueprint)
+
+    from .action import action_config as action_config_blueprint
+    app.register_blueprint(action_config_blueprint)
+
+    # from .greet import greet as greet_blueprint
+    # app.register_blueprint(greet_blueprint, url_prefix='/greet')
+
+    # from .calculate import calculate as calculate_blueprint
+    # app.register_blueprint(calculate_blueprint, url_prefix='/calculate')
+
     return app
