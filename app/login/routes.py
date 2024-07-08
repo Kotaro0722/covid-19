9f3fd05042dc8c8abe9843f5ae1acd2eb4f412a8
TanakaKotaro <kotaro.tanaka@4nonome.com>
2024-07-08 11:33:05 +0900

    main画面の処理を別ファイルに移動

--- app/login/routes.py
+++ app/login/routes.py
@@ -1,43 +1,9 @@
 from flask import render_template,request
 from . import login
-from . import login_config
 
 @login.route("/login")
 def login():
     return render_template("login.html")    
 
-
-@login_config.route("/",methods=["POST"])
-def login_config():
-    username=request.form["username"]
-    password=request.form["password"]
-    
-    is_admin=False
-    is_user=False
-    
-    admin_list=[
-        {"username":"admin","password":"admin1234"},
-    ]    
-    user_list=[
-        {"username":"user1234","password":"pass1234"},
-        {"username":"user5678","password":"pass5678"}, 
-    ]
-    
-    for user in admin_list:
-        if user["username"] == username and user["password"] == password:
-            is_admin=True
-    for user in user_list:
-        if user["username"] == username and user["password"] == password:
-            is_user=True
-            
-    
-    if is_admin:
-        return render_template("main_admin.html")
-    elif is_user:
-        return render_template("main_user.html")
-    else:
-        return render_template("login.html")
-    
-    
     
     
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,11 +1,36 @@
 from flask import render_template,request
-from . import main_user
-from . import main_admin
+from . import main
 
-@main_user.route("/")
-def main_user():
-    return render_template("")
 
-@main_admin.route("/admin")
-def main_admin():
-    return render_template("")
+@main.route("/",methods=["POST"])
+def login_config():
+    username=request.form["username"]
+    password=request.form["password"]
+    
+    is_admin=False
+    is_user=False
+    
+    admin_list=[
+        {"username":"admin","password":"admin1234"},
+    ]    
+    user_list=[
+        {"username":"user1234","password":"pass1234"},
+        {"username":"user5678","password":"pass5678"}, 
+    ]
+    
+    for user in admin_list:
+        if user["username"] == username and user["password"] == password:
+            is_admin=True
+    for user in user_list:
+        if user["username"] == username and user["password"] == password:
+            is_user=True
+            
+    
+    if is_admin:
+        return render_template("main_admin.html")
+    elif is_user:
+        return render_template("main_user.html")
+    else:
+        return render_template("login.html")
+    
+    
