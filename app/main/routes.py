9f80fe19f84aac693787208ff31fa88deb3c06f2
TanakaKotaro <kotaro.tanaka@4nonome.com>
2024-07-08 12:11:38 +0900

    MyDatabaseを追加

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,14 @@
 from flask import render_template,request
 from . import main
+from ..MyDatabase import my_open , my_query , my_close
 
+dsn = {
+    'host' : '172.30.0.10',  #ホスト名(IPアドレス)
+    'port' : '3306',        #mysqlの接続ポート番号
+    'user' : 'root',      #dbアクセスするためのユーザid
+    'password' : '1234',    #ユーザidに対応するパスワード
+    'database' : 'dbron04' #オープンするデータベース名
+}
 
 @main.route("/",methods=["POST"])
 def login_config():
@@ -27,6 +35,10 @@ def login_config():
             
     
     if is_admin:
+        dbcon,cur=my_open(**dsn)
+        sqlstring_related=f"""
+            SELECT * 
+        """
         return render_template("main_admin.html")
     elif is_user:
         return render_template("main_user.html")
