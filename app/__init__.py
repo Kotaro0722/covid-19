cea2e99ff45bd0beb9c0b159a09eee683110246c
yukino1822 <t122006@ed.sus.ac.jp>
2024-07-08 23:02:38 +0900

    変更

--- app/__init__.py
+++ app/__init__.py
@@ -8,5 +8,11 @@ def create_app():
     
     from .login import login_config as login_config_blueprint
     app.register_blueprint(login_config_blueprint)
+
+    from .action import action as action_blueprint
+    app.register_blueprint(action_blueprint)
+
+    from .action import action_config as action_config_blueprint
+    app.register_blueprint(action_config_blueprint)
     
     return app
