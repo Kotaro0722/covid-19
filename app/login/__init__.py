b77b6e0ff15a52a10ac97c22a533de5f8c7d0007
TanakaKotaro <kotaro.tanaka@4nonome.com>
2024-07-08 10:54:51 +0900

    作成中

--- app/login/__init__.py
+++ app/login/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
 login = Blueprint("login", __name__)
-login_config=Blueprint("login-config",__name__)
+login_config=Blueprint("login_config",__name__)
 
 from . import routes
